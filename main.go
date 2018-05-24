76b465045a2b5dacd045d51a8a5e5c877760f95e
froyobin <froyo.bin@gmail.com>
2018-05-24 16:53:31 +1000

    finish the coding

--- main.go
+++ main.go
@@ -9,54 +9,58 @@ import (
 	mathrand "math/rand"
 )
 
+var Gt0 *pbc.Element
+var Gt1 *pbc.Element
+var GBigR *pbc.Element
 
-type KeyPair struct{
-	Sk  SecretKey
-	Pk  PubKey
-	Vk  VKey
-	Attribute  []int
+type KeyPair struct {
+	Sk SecretKey
+	Tk TKey
+	Vk VKey
 }
 
-
-type EncryptData struct{
-
-	C0  *pbc.Element
-	C1  *pbc.Element
-	C2  *pbc.Element
-	C3  []*pbc.Element
-	C4  *pbc.Element
-
+type TransData struct {
+	C0P *pbc.Element
+	C1P *pbc.Element
 }
 
-
-type SecretKey struct{
-	Sk1 *pbc.Element
-	SK2 *pbc.Element
-	SK3 []*pbc.Element
-	SK4 []*pbc.Element
+type EncryptData struct {
+	C0 *pbc.Element
+	C1 *pbc.Element
+	C2 []*pbc.Element
+	C3 []*pbc.Element
+	C4 []*pbc.Element
 }
 
+type SecretKey struct {
+	Sk1       *pbc.Element
+	SK2       *pbc.Element
+	SK3       []*pbc.Element
+	SK4       []*pbc.Element
+	Attribute []*pbc.Element
+}
 
-type PubKey struct{
+type TKey struct {
 	Tk1 *pbc.Element
 	TK2 *pbc.Element
 	TK3 []*pbc.Element
 	TK4 []*pbc.Element
+	t0        *pbc.Element
 }
 
-type VKey struct{
+type VKey struct {
 	Vk1 *pbc.Element
 	VK2 *pbc.Element
 	VK3 []*pbc.Element
 	VK4 []*pbc.Element
+	t1        *pbc.Element
 }
 
-
-func Paraminit(rbits,qbits uint32)(*pbc.Pairing, map[string]*pbc.Element){
+func Paraminit(rbits, qbits uint32) (*pbc.Pairing, map[string]*pbc.Element) {
 
 	params := pbc.GenerateA(rbits, qbits)
 	pairing := params.NewPairing()
-	publicPara := make(map[string] *pbc.Element, 6)
+	publicPara := make(map[string]*pbc.Element, 6)
 
 	publicPara["g"] = pairing.NewG1().Rand()
 	publicPara["w"] = pairing.NewG1().Rand()
@@ -67,15 +71,15 @@ func Paraminit(rbits,qbits uint32)(*pbc.Pairing, map[string]*pbc.Element){
 	return pairing, publicPara
 }
 
-
 func GenerateSecretKey(Psize uint, attributenum int, pairing *pbc.Pairing,
-	parameters map[string] *pbc.Element, AList []int)(SecretKey){
+	parameters map[string]*pbc.Element) (SecretKey) {
 	//we firstly generate the ramdom values.
 	var thiskey SecretKey
 
+	Attribute := make([]*pbc.Element, attributenum, attributenum)
+
 	randsource := mathrand.New(mathrand.NewSource(time.Now().UnixNano()))
 	ONE := pairing.NewZr().Set1()
-
 
 	rZrlist := make([]*pbc.Element, attributenum, attributenum)
 
@@ -86,14 +90,21 @@ func GenerateSecretKey(Psize uint, attributenum int, pairing *pbc.Pairing,
 
 	alphaZr := pairing.NewZr().MulBig(ONE, alpha)
 
+	for i := 0; i < len(Attribute); i++ {
 
-	for i,_ := range(rZrlist) {
+		tmp := new(big.Int).Rand(randsource, new(big.Int).Lsh(big.NewInt(1), Psize))
+
+		Attribute[i] = pairing.NewZr().MulBig(ONE, tmp)
+	}
+
+	for i, _ := range (rZrlist) {
 		r := new(big.Int).Rand(randsource, new(big.Int).Lsh(big.NewInt(1), Psize))
 		rZr := pairing.NewZr().MulBig(ONE, r)
 		rZrlist[i] = rZr
 	}
-
-
+	r1 := new(big.Int).Rand(randsource, new(big.Int).Lsh(big.NewInt(1), Psize))
+	bigr := pairing.NewZr().MulBig(ONE, r1)
+	GBigR = bigr
 	g := parameters["g"]
 	w := parameters["w"]
 	u := parameters["u"]
@@ -102,38 +113,25 @@ func GenerateSecretKey(Psize uint, attributenum int, pairing *pbc.Pairing,
 
 	//fixme, we update the e(g,g)^{alpha} here
 
-	pairingalpha := pairing.NewGT().PowZn(pairing.NewGT().Pair(g,g), alphaZr)
+	pairingalpha := pairing.NewGT().PowZn(pairing.NewGT().Pair(g, g), alphaZr)
 	parameters["pairingalpha"] = pairingalpha
 
+	thiskey.Sk1 = pairing.NewG1().Pow2Zn(g, alphaZr, w, bigr)
+	thiskey.SK2 = pairing.NewG1().PowZn(g, bigr)
 
-	thiskey.Sk1 = pairing.NewG1().Pow2Zn(g, alphaZr, w, rZrlist[0])
-	thiskey.SK2 = pairing.NewG1().PowZn(g, rZrlist[0])
-
-
-
-
-
-	for i:=1;i < len(rZrlist);i++ {
-
+	for i := 0; i < len(rZrlist); i++ {
 		thiskey.SK3[i] = pairing.NewG1().PowZn(g, rZrlist[i])
-		ael := pairing.NewZr().Set1()
-		if AList[i] == 1{
-			ael = pairing.NewZr().Set1()
-		}else{
-			ael = pairing.NewZr().Set0()
-		}
-		uh := pairing.NewG1().Pow2Zn(u, ael, h, pairing.NewZr().Set1())
-		uh =uh.PowZn(uh, rZrlist[i])
-		thiskey.SK4[i] = pairing.NewG1().Pow2Zn(uh,rZrlist[i],v,
-			pairing.NewZr().Neg(rZrlist[0]))
+		uh := pairing.NewG1().Pow2Zn(u, Attribute[i], h, pairing.NewZr().Set1())
+		thiskey.SK4[i] = pairing.NewG1().Pow2Zn(uh, rZrlist[i], v, pairing.NewZr().Neg(bigr))
 	}
-
+	thiskey.Attribute = Attribute
 	return thiskey
 }
 
-func GenerateTranandVKey(Psize uint, pairing *pbc.Pairing, k int, SK SecretKey)(PubKey, VKey){
+func GenerateTranandVKey(Psize uint, pairing *pbc.Pairing, k int, SK SecretKey,
+	parameters map[string]*pbc.Element) (TKey, VKey) {
 
-	var myPuk PubKey
+	var myPuk TKey
 	var myVk VKey
 	randsource := mathrand.New(mathrand.NewSource(time.Now().UnixNano()))
 	t0big := new(big.Int).Rand(randsource, new(big.Int).Lsh(big.NewInt(1), Psize))
@@ -141,34 +139,38 @@ func GenerateTranandVKey(Psize uint, pairing *pbc.Pairing, k int, SK SecretKey)(
 	ONE := pairing.NewZr().Set1()
 	t0 := pairing.NewZr().MulBig(ONE, t0big)
 	t1 := pairing.NewZr().MulBig(ONE, t1big)
-
+	Gt0 = t0
+	Gt1 = t1
 	myPuk.Tk1 = pairing.NewG1().PowZn(SK.Sk1, t0)
 	myPuk.TK2 = pairing.NewG1().PowZn(SK.SK2, t0)
 	myPuk.TK3 = make([]*pbc.Element, k, k)
 	myPuk.TK4 = make([]*pbc.Element, k, k)
 
-	myVk.Vk1 = pairing.NewG1().PowZn(SK.Sk1, t0)
-	myVk.VK2 = pairing.NewG1().PowZn(SK.SK2, t0)
+	myVk.Vk1 = pairing.NewG1().PowZn(myPuk.Tk1, t1)
+	myVk.VK2 = pairing.NewG1().PowZn(myPuk.TK2, t1)
 	myVk.VK3 = make([]*pbc.Element, k, k)
 	myVk.VK4 = make([]*pbc.Element, k, k)
 
-
-	for i:=1;i < k; i++ {
+	for i := 0; i < k; i++ {
 		myPuk.TK3[i] = pairing.NewG1().PowZn(SK.SK3[i], t0)
 		myPuk.TK4[i] = pairing.NewG1().PowZn(SK.SK4[i], t0)
-		myVk.VK3[i] = pairing.NewG1().PowZn(SK.SK3[i], t1)
-		myVk.VK4[i] = pairing.NewG1().PowZn(SK.SK4[i], t1)
-
+		myVk.VK3[i] = pairing.NewG1().PowZn(myPuk.TK3[i], t1)
+		myVk.VK4[i] = pairing.NewG1().PowZn(myPuk.TK4[i], t1)
 
 	}
+
+	verfiy_keys(pairing, myPuk, myVk, parameters["g"], t1, k)
+
+	myPuk.t0 = t0
+	myVk.t1 = t1
 
 	return myPuk, myVk
 
 }
 
-
-func Encryption(pairing *pbc.Pairing,parameters map[string] *pbc.Element,
-	secret *pbc.Element,message *pbc.Element, key SecretKey, attr []int)(EncryptData){
+func Encryption(pairing *pbc.Pairing, parameters map[string]*pbc.Element,
+	message *pbc.Element, key SecretKey, num int) (EncryptData,
+	[]*pbc.Element, []*pbc.Element) {
 
 	//msg := []byte(message)
 
@@ -182,46 +184,270 @@ func Encryption(pairing *pbc.Pairing,parameters map[string] *pbc.Element,
 	//msgEl := pairing.NewZr().SetCompressedBytes(msg)
 	//org := msgEl.CompressedBytes()
 
-	//fixme for simplicity we set the sum of u_i instead of the u_i
-	usum := pairing.NewZr().Rand()
+	uu := make([]*pbc.Element, num, num)
+	vv := make([]*pbc.Element, num, num)
+
+	secret := pairing.NewZr().Set0()
+	for i := 0; i < num; i++ {
+		uu[i] = pairing.NewZr().Rand()
+		vv[i] = pairing.NewZr().Rand()
+		secret = secret.ThenAdd(vv[i])
+	}
+
+	C2 := make([] *pbc.Element, num, num)
+	C3 := make([] *pbc.Element, num, num)
+	C4 := make([] *pbc.Element, num, num)
+
+	C0 := pairing.NewGT().PowZn(alpha, secret).ThenMul(message)
 
 
-	C3 := make([] *pbc.Element,len(attr),len(attr))
 
-	C0 := pairing.NewGT().PowZn(alpha, secret).ThenMulZn(message)
+
 	C1 := pairing.NewG1().PowZn(g, secret)
-	C2 := pairing.NewG1().Mul(pairing.NewG1().PowZn(w,secret), pairing.NewG1().PowZn(v, usum))
 
-
-
-	ael := pairing.NewZr()
-	for i:=0;i<len(attr);i++ {
-
-		if attr[i] == 1 {
-			ael = pairing.NewZr().Set1()
-		} else {
-			ael = pairing.NewZr().Set0()
-		}
-
-		C3_1 := pairing.NewG1().PowZn(u,ael).ThenMul(h)
-		C3[i]= pairing.NewG1().PowZn(C3_1, pairing.NewZr().Neg(secret))
+	for i := 0; i < num; i++ {
+		C2[i] = pairing.NewG1().Mul(pairing.NewG1().PowZn(w, vv[i]), pairing.NewG1().PowZn(v,
+			uu[i]))
+		tmp := pairing.NewG1().Pow2Zn(u, key.Attribute[i], h, pairing.NewZr().Set1())
+		C3[i] = pairing.NewG1().PowZn(tmp, pairing.NewZr().Neg(uu[i]))
+		C4[i] = pairing.NewG1().PowZn(g, uu[i])
 	}
-	C4 := pairing.NewG1().PowZn(g, secret)
 
-    encrypt := EncryptData{
-    	C0,
-    	C1,
-    	C2,
-    	C3,
-    	C4,
-
+	encrypt := EncryptData{
+		C0,
+		C1,
+		C2,
+		C3,
+		C4,
 	}
+	return encrypt, uu, vv
+}
+
+func Transform(pairing *pbc.Pairing, parameters map[string]*pbc.Element, endata EncryptData,
+	keypair KeyPair, k int, uu, vv []*pbc.Element) (TransData) {
+	C1 := endata.C1
+	TK := keypair.Tk
+	VK := keypair.Vk
+
+	above := pairing.NewGT().Pair(C1, TK.Tk1)
+	above1 := pairing.NewGT().Pair(C1, VK.Vk1)
+
+	output0 := pairing.NewGT().Set1()
+	output1 := pairing.NewGT().Set1()
+	for i := 0; i < k; i++ {
+
+		first := pairing.NewGT().Pair(endata.C2[i], TK.TK2)
+		second := pairing.NewGT().Pair(endata.C3[i], TK.TK3[i])
+		third := pairing.NewGT().Pair(endata.C4[i], TK.TK4[i])
+
+		first1 := pairing.NewGT().Pair(endata.C2[i], VK.VK2)
+		second1 := pairing.NewGT().Pair(endata.C3[i], VK.VK3[i])
+		third1 := pairing.NewGT().Pair(endata.C4[i], VK.VK4[i])
+
+		result2 := pairing.NewGT().Mul(first1, second1).ThenMul(third1)
+
+		//result := pairing.NewGT().Mul(tmpresult, first)
+		result := pairing.NewGT().Mul(first, second).ThenMul(third)
+
+		output0 = output0.ThenMul(result)
+		output1 = output1.ThenMul(result2)
+	}
+	secret := pairing.NewZr().Set0()
+	for _, el := range (vv) {
+		secret.ThenAdd(el)
+	}
+
+	C0P := above.ThenDiv(output0)
+	C1P := above1.ThenDiv(output1)
+	egg := parameters["pairingalpha"]
+
+	outfinal := pairing.NewGT().PowZn(egg, pairing.NewZr().Mul(Gt0, secret))
+	if C0P.Equals(outfinal) {
+		fmt.Println("pass the C0p check")
+	} else {
+		fmt.Println("fail the C0P check")
+	}
+
+	if C1P.Equals(pairing.NewGT().PowZn(outfinal, Gt1)) {
+		fmt.Println("pass the C1P check")
+	} else {
+		fmt.Println("fail the C1P check")
+	}
+
+	encrypt := TransData{
+		C0P,
+		C1P,
+	}
+
 	return encrypt
 }
 
+func Verify(pairing *pbc.Pairing, data TransData, key VKey) {
+	if data.C1P.Equals(pairing.NewGT().PowZn(data.C0P, key.t1)){
+		fmt.Println("pass the key verfication")
+	} else {
+		fmt.Println("fail the key verification")
+	}
+
+}
+
+func verfiy_keys(pairing *pbc.Pairing, tk TKey, vk VKey, g, t1 *pbc.Element, k int) {
+	p1 := pairing.NewGT()
+	p2 := pairing.NewGT()
+	gt1 := pairing.NewG1().PowZn(g, t1)
+
+	vk1 := vk.Vk1
+	tk1 := tk.Tk1
+
+	vk2 := vk.VK2
+	tk2 := tk.TK2
+
+	vk3 := vk.VK3
+	tk3 := tk.TK3
+
+	vk4 := vk.VK4
+	tk4 := tk.TK4
+
+	p1 = p1.Pair(vk1, g)
+	p2 = p2.Pair(tk1, gt1)
+	if (p1.Equals(p2) == false) {
+		fmt.Println("error!!!!")
+		return
+	}
+
+	p1 = p1.Pair(vk2, g)
+	p2 = p2.Pair(tk2, gt1)
+	if (p1.Equals(p2) == false) {
+		fmt.Println("error!!!!")
+		return
+	}
+
+	for i := 0; i < k; i++ {
+
+		p1 = p1.Pair(vk3[i], g)
+		p2 = p2.Pair(tk3[i], gt1)
+		if (p1.Equals(p2) == false) {
+			fmt.Println("error!!!!")
+			return
+		}
+	}
+
+	for i := 0; i < k; i++ {
+
+		p1 = p1.Pair(vk4[i], g)
+		p2 = p2.Pair(tk4[i], gt1)
+		if (p1.Equals(p2) == false) {
+			fmt.Println("error!!!!")
+			return
+		}
+	}
+	fmt.Println("pass the check")
+	return
+}
+
+func Decrypt(pairing *pbc.Pairing,data EncryptData,trans TransData, key TKey)(*pbc.Element){
+	C0 := data.C0
+	t0 := key.t0
+	C0P := trans.C0P
+
+	//oOvert0 := pairing.NewZr().Div(pairing.NewZr().Set1(), t0)
+	bottom := pairing.NewGT().PowZn(C0P, pairing.NewZr().Invert(t0))
+	msg := pairing.NewGT().Div(C0, bottom)
+	return msg
 
 
-func Transform(endata EncryptData, key VKey){
+}
+
+
+func play_test(pairing *pbc.Pairing) {
+
+	a := pairing.NewZr().Rand()
+	b := pairing.NewZr().Rand()
+	c := pairing.NewZr().Rand()
+
+	x := pairing.NewG1().Rand()
+	y := pairing.NewG1().Rand()
+	z := pairing.NewG1().Rand()
+
+	first := pairing.NewG1().Pow2Zn(x, a, y, b)
+	second := pairing.NewG1().PowZn(z, c)
+
+	p1 := pairing.NewGT().Pair(first, second)
+
+	ac := pairing.NewZr().Mul(a, c)
+	bc := pairing.NewZr().Mul(b, c)
+
+	first1 := pairing.NewG1().Pow2Zn(x, ac, y, bc)
+
+	p2 := pairing.NewGT().Pair(first1, z)
+
+	first2 := pairing.NewGT().Pair(pairing.NewG1().PowZn(x, a), pairing.NewG1().PowZn(z, c))
+	second2 := pairing.NewGT().Pair(pairing.NewG1().PowZn(y, b), pairing.NewG1().PowZn(z, c))
+
+	p3 := pairing.NewGT().Mul(first2, second2)
+
+	//p1 = e(x^a*y^b,z^c)=e(x^(ac)*y^(bc),z)=e(x^a,z^c)*e(y^b,z^c)
+
+	xa := pairing.NewG1().PowZn(x, a)
+	yb := pairing.NewG1().PowZn(y, b)
+	zc := pairing.NewG1().PowZn(z, c)
+
+	first3 := pairing.NewG1().Add(xa, yb)
+
+	p4 := pairing.NewGT().Pair(first3, zc)
+
+	fmt.Println("===================")
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Println(p3)
+	fmt.Println(p4)
+	fmt.Println("===================")
+
+	//e(x^a,y^b) = e(y^(-b),x^a)
+
+	p5 := pairing.NewGT().Pair(pairing.NewG1().PowZn(x, a), pairing.NewG1().PowZn(y, b))
+
+	p6 := pairing.NewGT().Pair(pairing.NewG1().PowZn(y, pairing.NewZr().Neg(b)),
+		pairing.NewG1().PowZn(x, a))
+
+	fmt.Println("===================")
+	fmt.Println(p5)
+	fmt.Println(p6)
+	fmt.Println("===================")
+
+	//e(a,b)*e(b,a^(-1)) = 1
+	one := pairing.NewZr().Set1()
+	baseg := pairing.NewG1().Rand()
+	first4 := pairing.NewGT().Pair(xa, baseg)
+	second4 := pairing.NewGT().Pair(baseg, pairing.NewG1().PowZn(xa, pairing.NewZr().Neg(one)))
+
+	fmt.Println(pairing.NewGT().Mul(first4, second4))
+	fmt.Println(pairing.NewGT().Pair(pairing.NewG1().Set1(), baseg))
+
+	ax := pairing.NewG1().MulZn(x, a)
+	by := pairing.NewG1().MulZn(y, b)
+
+	o1 := pairing.NewGT().Pair(ax, by)
+
+	o2 := pairing.NewGT().Pair(x, y)
+	o2 = pairing.NewGT().PowZn(o2, pairing.NewZr().Mul(a, b))
+
+	fmt.Println(o1)
+	fmt.Println(o2)
+
+	x_a := pairing.NewG1().PowZn(x, a)
+	y_b := pairing.NewG1().PowZn(y, b)
+
+	o3 := pairing.NewGT().Pair(x_a, y_b)
+	o3p := pairing.NewGT().Pair(y_b, x_a)
+	fmt.Println("o3", o3)
+	fmt.Println("o3p", o3p)
+
+	fmt.Println("---------")
+	fmt.Println(ax)
+	fmt.Println(x_a)
+
+
 
 
 
@@ -233,47 +459,46 @@ func main() {
 	AttibuteNum := flag.Int("atrnum", 5, "Number of the attributes")
 	Psize := flag.Int("ps", 1024, "the size of group P")
 	flag.Parse()
-	pairing, parameters := Paraminit(160,512)
+	pairing, parameters := Paraminit(160, 512)
 
-	Attribute := make([]int, *AttibuteNum, *AttibuteNum)
-	randsource := mathrand.New(mathrand.NewSource(time.Now().UnixNano()))
-	r1 := mathrand.New(randsource)
+	//randsource := mathrand.New(mathrand.NewSource(time.Now().UnixNano()))
+	//r1 := mathrand.New(randsource)
 
+	mysecretkey := GenerateSecretKey(uint(*Psize), *AttibuteNum, pairing, parameters)
 
-
-	for i:=0;i<*AttibuteNum;i++{
-		value := r1.Intn(2)
-
-		Attribute[i] = value
-	}
-
-	mysecretkey := GenerateSecretKey(uint(*Psize), *AttibuteNum, pairing,
-		parameters,
-		Attribute)
-
-	Mypub, Myvkey := GenerateTranandVKey(uint(*Psize), pairing, *AttibuteNum, mysecretkey)
+	Mypub, Myvkey := GenerateTranandVKey(uint(*Psize), pairing, *AttibuteNum, mysecretkey,
+		parameters)
 
 	mykeypair := KeyPair{
 		mysecretkey,
 		Mypub,
 		Myvkey,
-		Attribute,
 	}
 
 	//fixme, we do not split the secret into matrix
-	u := pairing.NewZr().Rand()
-	msg := pairing.NewZr().SetBig(big.NewInt(12))
-	encryptodata := Encryption(pairing,parameters, u,msg, mykeypair.Sk, Attribute)
+	msg := pairing.NewGT().Rand()
+	encryptodata, uu, vv := Encryption(pairing, parameters, msg, mykeypair.Sk, *AttibuteNum)
 
-	Transform(encryptodata, mykeypair.Vk)
+	transdata := Transform(pairing, parameters, encryptodata, mykeypair, *AttibuteNum, uu, vv)
 
-
-
-    fmt.Println("thiskey",mysecretkey.SK4)
-    fmt.Println("thiskey",mykeypair.Vk.VK4)
-	fmt.Println(encryptodata.C0)
+	Verify(pairing, transdata, mykeypair.Vk)
 
 
+	msgout := Decrypt(pairing, encryptodata, transdata, mykeypair.Tk)
+	fmt.Println(msgout)
 
+
+	fmt.Println("----------------ANSWER---------")
+	fmt.Println("org:", msg.String())
+	fmt.Println("recoverd: ", msgout.String())
+
+	if msg.Equals(msgout){
+		fmt.Println("Successfull")
+	}else{
+		fmt.Println("Protocol Failed")
+	}
+
+	fmt.Println("-------------------------------")
+	//play_test(pairing)
 
 }
